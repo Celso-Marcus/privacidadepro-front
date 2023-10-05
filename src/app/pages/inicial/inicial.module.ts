f95baacf75f5743218f72b00fae5a186082e12b3
Celso Brito <celso.botelho14@gmail.com>
2023-10-04 21:59:21 -0300

    feat: inicial routing module

--- src/app/pages/inicial/inicial.module.ts
+++ src/app/pages/inicial/inicial.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { CoreModule } from 'src/app/core/core.module';
 import { ComponentsModule } from '../components/components.module';
+import { InicialRouteRoutingModule } from './inicial-route-routing.module';
 
 
 
@@ -12,7 +13,8 @@ import { ComponentsModule } from '../components/components.module';
     CommonModule,
     SharedModule,
     CoreModule,
-    ComponentsModule
+    ComponentsModule,
+    InicialRouteRoutingModule
   ]
 })
 export class InicialModule { }
