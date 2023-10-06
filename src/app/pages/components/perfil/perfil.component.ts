c4844be66cfe753738df78e50bead790321dd0f5
marksv21 <marvmms@gmail.com>
2023-10-05 23:57:14 -0300

    create perfil

---
+++ src/app/pages/components/perfil/perfil.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-perfil',
+  templateUrl: './perfil.component.html',
+  styleUrls: ['./perfil.component.scss']
+})
+export class PerfilComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+}
