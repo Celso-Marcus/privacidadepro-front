4b6cc8f35b21f9d0b8b0f79e17b96bf2a8222ecf
marksv21 <marvmms@gmail.com>
2023-11-15 17:45:52 -0300

    fix: resolvido a logica do quiz

--- src/app/pages/inicial/inicial-quiz-lpgd/inicial-criar-quiz-lpgd/inicial-criar-quiz-lpgd.component.ts
+++ src/app/pages/inicial/inicial-quiz-lpgd/inicial-criar-quiz-lpgd/inicial-criar-quiz-lpgd.component.ts
@@ -26,124 +26,123 @@ export class InicialCriarQuizLpgdComponent {
   answers = new Array(24).fill("0");
   dpoName = 'teste marcus';
   createdAt = new Date();
+  maturityResult: number = 0;
   cancelarQuiz() {
     this.naomostrar.emit();// Altera a variável para mostrar o componente do Quiz
   }
   constructor( private router: Router , private quizService: QuizService){}
 
   handleQuiz = async () => {
-        let maturityResult = 0;
-        for(let i in this.answers){
-            if ( parseInt(i) < 6 ){
+        for(let i = 0; i < this.answers.length; i++){
+            if ( i < 6 ){
                 switch(this.answers[i]){
-                    case '0':
-                        maturityResult += 0;
+                    case 0:
+                        this.maturityResult += 0;
                     break;
-                    case '1':
-                        maturityResult += 0.25*0.25;
+                    case 1:
+                        this.maturityResult += (0.25*0.25);
                     break;
-                    case '2':
-                        maturityResult += 0.5*0.25;
+                    case 2:
+                        this.maturityResult += (0.5*0.25);
                     break;
-                    case '3':
-                        maturityResult += 1*0.25;
+                    case 3:
+                        this.maturityResult += (1*0.25);
                     break;
-
-                }
-            }else if (parseInt(i) >= 6 && parseInt(i) >=11 ){
+                  }
+            }else if (i >= 6 && i <= 11) {
                 switch(this.answers[i]){
-                    case '0':
-                        maturityResult += 0;
+                    case 0:
+                        this.maturityResult += 0;
                     break;
-                    case '1':
-                        maturityResult += 0.25*0.25;
+                    case 1:
+                        this.maturityResult += (0.25*0.25);
                     break;
-                    case '2':
-                        maturityResult += 0.5*0.25;
+                    case 2:
+                        this.maturityResult += (0.5*0.25);
                     break;
-                    case '3':
-                        maturityResult += 1*0.25;
+                    case 3:
+                        this.maturityResult += (1*0.25);
                     break;
                 }
-            }else if (parseInt(i) == 12 && parseInt(i) == 13 ){
+            }else if (i >= 12 && i <= 13){
                 switch(this.answers[i]){
-                    case '0':
-                        maturityResult += 0;
+                    case 0:
+                        this.maturityResult += 0;
                     break;
-                    case '1':
-                        maturityResult += 0.25*0.1;
+                    case 1:
+                        this.maturityResult += (0.25*0.1);
                     break;
-                    case '2':
-                        maturityResult += 0.5*0.1;
+                    case 2:
+                        this.maturityResult += (0.5*0.1);
                     break;
-                    case '3':
-                        maturityResult += 1*0.1;
+                    case 3:
+                        this.maturityResult += (1*0.1);
                     break;
                 }
-            }else if (parseInt(i) >= 14 && parseInt(i) >= 16 ){
+            }else if (i >= 14 && i <= 16){
                 switch(this.answers[i]){
-                    case '0':
-                        maturityResult += 0;
+                    case 0:
+                        this.maturityResult += 0;
                     break;
-                    case '1':
-                        maturityResult += 0.25*0.05;
+                    case 1:
+                        this.maturityResult += (0.25*0.05);
                     break;
-                    case '2':
-                        maturityResult += 0.5*0.05;
+                    case 2:
+                        this.maturityResult += (0.5*0.05);
                     break;
-                    case '3':
-                        maturityResult += 1*0.05;
+                    case 3:
+                        this.maturityResult += (1*0.05);
                     break;
                 }
-            }else if (this.answers.includes(17)){
+            }else if (i === 17){
                 switch(this.answers[i]){
-                    case '0':
-                        maturityResult += 0;
+                    case 0:
+                        this.maturityResult += 0;
                     break;
-                    case '1':
-                        maturityResult += 0.25*0.05;
+                    case 1:
+                        this.maturityResult += (0.25*0.05);
                     break;
-                    case '2':
-                        maturityResult += 0.5*0.05;
+                    case 2:
+                        this.maturityResult += (0.5*0.05);
                     break;
-                    case '3':
-                        maturityResult += 1*0.05;
+                    case 3:
+                        this.maturityResult += (1*0.05);
                     break;
                 }
-            }else if (parseInt(i) >= 18 && parseInt(i) >= 20){
+            }else if (i >= 18 && i <= 20){
                 switch(this.answers[i]){
-                    case '0':
-                        maturityResult += 0;
+                    case 0:
+                        this.maturityResult += 0;
                     break;
-                    case '1':
-                        maturityResult += 0.25*0.15;
+                    case 1:
+                        this.maturityResult += (0.25*0.15);
                     break;
-                    case '2':
-                        maturityResult += 0.5*0.15;
+                    case 2:
+                        this.maturityResult += (0.5*0.15);
                     break;
-                    case '3':
-                        maturityResult += 1*0.15;
+                    case 3:
+                        this.maturityResult += (1*0.15);
                     break;
                 }
-            }else if (parseInt(i) >= 21 && parseInt(i) >= 23){
+            }else if (i >= 21 && i <= 23){
                 switch(this.answers[i]){
-                    case '0':
-                        maturityResult += 0;
+                    case 0:
+                        this.maturityResult += 0;
                     break;
-                    case '1':
-                        maturityResult += 0.25*0.15;
+                    case 1:
+                        this.maturityResult += (0.25*0.15);
                     break;
-                    case '2':
-                        maturityResult += 0.5*0.15;
+                    case 2:
+                        this.maturityResult += (0.5*0.15);
                     break;
-                    case '3':
-                        maturityResult += 1*0.15;
+                    case 3:
+                        this.maturityResult += (1*0.15);
                     break;
                 }
             }
         }
-        maturityResult = maturityResult * 10;
-        if (maturityResult < 2.99) {
+        this.maturityResult = this.maturityResult * 10;
+        if (this.maturityResult < 2.99) {
             const textResult = 'Iniciante';
             const raw = {
                 answers: this.answers.toString(),
@@ -162,9 +161,7 @@ export class InicialCriarQuizLpgdComponent {
                     alert(result.message);
                 }
             });
-
-        }
-        else if ((maturityResult >= 3.00) && (maturityResult <= 4.99)){
+        }else if ((this.maturityResult >= 3.00) && (this.maturityResult <= 4.99)){
             const textResult = 'Básico';
             const raw = {
                 answers: this.answers.toString(),
@@ -183,8 +180,7 @@ export class InicialCriarQuizLpgdComponent {
                 alert(result.message);
               }
             });
-        }
-        else if ((maturityResult >= 5.00) && (maturityResult <= 6.99)){
+        }else if ((this.maturityResult >= 5.00) && (this.maturityResult <= 6.99)){
             const textResult = 'Intermediário';
             const raw: CreateQuiz = {
                 answers: this.answers.toString(),
@@ -203,8 +199,7 @@ export class InicialCriarQuizLpgdComponent {
                 alert(result.message);
               }
             });
-        }
-        else if ((maturityResult >= 7.00) && (maturityResult <= 8.99)){
+        }else if ((this.maturityResult >= 7.00) && (this.maturityResult <= 8.99)){
             const textResult = 'Intermediário Superior';
             const raw = {
                 answers: this.answers.toString(),
@@ -222,8 +217,7 @@ export class InicialCriarQuizLpgdComponent {
                 alert(result.message);
               }
             });
-        }
-        else if (maturityResult >= 9.00){
+        }else if (this.maturityResult >= 9.00){
             const textResult = 'Avançado';
             const raw = {
                 answers: this.answers.toString(),
